63ca3af7ab04f4c274a29077ad5c158f11a73602
Jung SeokJoon <eshc.joon@gmail.com>
2023-01-05 12:18:34 +0900

    [FEATURE] #1 MovieUiModel 추가

---
+++ app/src/main/java/com/eshc/moviesearchapp/ui/model/MovieUiModel.kt
@@ -0,0 +1,9 @@
+package com.eshc.moviesearchapp.ui.model
+
+data class MovieUiModel(
+    val title : String,
+    val publishDate : String,
+    val image : String,
+    val userRating : Float,
+    val link : String
+)
