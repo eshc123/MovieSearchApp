5d68f315d957631ee3d67f76ddb8fecffa0f188e
Jung SeokJoon <eshc.joon@gmail.com>
2023-01-05 13:40:32 +0900

    [FEATURE] #2 response model 추가 - channel , movie

---
+++ app/src/main/java/com/eshc/moviesearchapp/data/api/model/Movie.kt
@@ -0,0 +1,11 @@
+package com.eshc.moviesearchapp.data.api.model
+
+data class Movie(
+    val title : String,
+    val pubDate : String,
+    val image : String,
+    val userRating : Float,
+    val subtitle : String,
+    val director : String,
+    val actor : String
+)
