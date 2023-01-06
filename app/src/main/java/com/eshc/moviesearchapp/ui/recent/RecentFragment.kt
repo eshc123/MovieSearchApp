1f2bbf08743f6a4c6f91bcb239568cb2b54dffe0
Jung SeokJoon <eshc.joon@gmail.com>
2023-01-06 14:30:37 +0900

    [FEATURE] #3 RecentFragment 수정 - viewModel 적용

--- app/src/main/java/com/eshc/moviesearchapp/ui/recent/RecentFragment.kt
+++ app/src/main/java/com/eshc/moviesearchapp/ui/recent/RecentFragment.kt
@@ -5,20 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.eshc.moviesearchapp.databinding.FragmentRecentBinding
 import com.eshc.moviesearchapp.ui.adapter.RecentAdapter
+import com.eshc.moviesearchapp.ui.movie.MovieViewModel
 import com.google.android.flexbox.FlexDirection
 import com.google.android.flexbox.FlexboxLayoutManager
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class RecentFragment : Fragment() {
     private var _binding: FragmentRecentBinding? = null
     private val binding get() = _binding
 
+    private val viewModel : MovieViewModel by activityViewModels()
+
     private val recentAdapter: RecentAdapter by lazy {
         RecentAdapter(
             recentClickListener = {
+                setSearchQuery(it)
+                searchMovie()
                 backToMovieFragment()
             }
         )
@@ -55,10 +66,24 @@ class RecentFragment : Fragment() {
     }
 
     private fun initObserver() {
-
+        lifecycleScope.launch{
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.recents.collectLatest { recents ->
+                    recentAdapter.submitList(recents)
+                }
+            }
+        }
     }
 
     private fun backToMovieFragment() {
         findNavController().navigateUp()
+    }
+
+    private fun setSearchQuery(recent : String){
+        viewModel.query.value = recent
+    }
+
+    private fun searchMovie() {
+        viewModel.setMovies()
     }
 }
