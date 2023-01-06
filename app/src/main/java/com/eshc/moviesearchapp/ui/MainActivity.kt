b66101480bfa70f04d86e351f818a68b5798911a
Jung SeokJoon <eshc.joon@gmail.com>
2023-01-06 15:40:07 +0900

    [FEATURE] MainActivity - AndroidEntryPoint 추가

--- app/src/main/java/com/eshc/moviesearchapp/ui/MainActivity.kt
+++ app/src/main/java/com/eshc/moviesearchapp/ui/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.eshc.moviesearchapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
